75fdb19a0a9746ebd5e38b64ac64287bec1ea77a
Shahin Alam Kiron <91818093+ShahinAlamK@users.noreply.github.com>
2023-07-28 19:18:12 +0600

    Update translator_controller.dart

--- lib/controller/translator_controller.dart
+++ lib/controller/translator_controller.dart
@@ -7,10 +7,7 @@ class TranslatorController extends GetxController{
 
   Future<dynamic>fetchTranslator()async{
     Uri uri=Uri.parse("https://google-translate1.p.rapidapi.com/language/translate/v2");
-    Map<String,String>header={
-      "X-RapidAPI-Key":"2604fd11a1msh4657444b52afde6p1b19a6jsnf7f6af0996a2",
-      "content-type":"application/x-www-form-urlencoded",
-    };
+    Map<String,String>header={}
     final body={
       "q":"Hello World",
       "target":"bn",
